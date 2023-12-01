8c1dd0838614f2613c97755adf93ca06b05d9012
heyej <78939576+heyej@users.noreply.github.com>
2023-12-01 17:41:41 -0600

    Update test.py

--- test.py
+++ test.py
@@ -5,15 +5,15 @@ import xarray as xr
 
 def main():
         st.title("NetCDF Visualization Example")
-    st.header('Test')
-
-    filename = 'wind_global.nc'
-    geojson = 'https://github.com/opengeos/leafmap/raw/master/examples/data/countries.geojson'
-    m = leafmap.Map(layers_control=True)
-    m.add_basemap('CartoDB.DarkMatter')
-    #m.add_velocity(filename, zonal_speed='u_wind', meridional_speed='v_wind')
-    m.add_netcdf(filename, variables=None, port='default', layer_name='NetCDF layer', shift_lon=True, lat='lat', lon='lon')
-    m.to_streamlit()
+        st.header('Test')
+        
+        filename = 'wind_global.nc'
+        geojson = 'https://github.com/opengeos/leafmap/raw/master/examples/data/countries.geojson'
+        m = leafmap.Map(layers_control=True)
+        m.add_basemap('CartoDB.DarkMatter')
+        #m.add_velocity(filename, zonal_speed='u_wind', meridional_speed='v_wind')
+        m.add_netcdf(filename, variables=None, port='default', layer_name='NetCDF layer', shift_lon=True, lat='lat', lon='lon')
+        m.to_streamlit()
 
 if __name__ == "__main__":
     main()
