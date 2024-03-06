12141a8942fb9540a681c175fc81c07f5fa5e31b
Gus <heyej@ier.unam.mx>
2024-03-06 15:02:37 -0600

    Actualización 2

---
+++ pages/1📍_Locations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+#
+#  streamlit.py
+#
+
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import streamlit as st
+
+def main():
+    #st.set_page_config(layout="wide")
+    st.sidebar.title("Navigation")
+
+    st.title("Atmospheric mesoscale modeling to simulate annual and seasonal wind speeds for wind energy production in Mexico")
+    st.header('Reference')
+
+if __name__ == "__main__":
+    main()
